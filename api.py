c074405a862daa9078f64b2fed567aaf49b88e10
Shaun Lee <shaunleeyx@gmail.com>
2022-11-19 17:29:00 -0800

    Update api.py

--- api.py
+++ api.py
@@ -3,17 +3,16 @@ import json
 routingvalue = "na1.api.riotgames.com"
 region = "https://americas.api.riotgames.com"
 key = "RGAPI-2b2982b4-6279-4585-b728-fa5439724fc0"
-apiurl = "/lol/match/v5/matches/by-puuid/{puuid}/ids"
+matchhistoryapi = "/lol/match/v5/matches/by-puuid/{puuid}/ids"
 puuid = "VZGeiLRo-jnyT4qa3eSNM39hdOr6wyV7AxCHkHE66ocileMfKBtMSQznhFHpU_jmqNYeUWEq-mORiQ"
-parameters = {
-    "region": routingvalue
-}
-url = region + apiurl.format(puuid=puuid) + "?api_key=" + key
-response = requests.get(url)
+matchv5url = region + matchhistoryapi.format(puuid=puuid) + "?api_key=" + key
+
+matchhistoryrequest = requests.get(matchv5url)
+matchhistory = matchhistoryrequest.json()
+
+
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
-
-jprint(response.json())
